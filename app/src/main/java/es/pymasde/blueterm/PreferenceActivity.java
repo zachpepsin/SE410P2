baa8a3dbce1d327814cebd943bfcbf5eb55d7571
Brett Rosen <Brejuro23@gmail.com>
2016-03-14 23:33:21 -0400

    Added PreferenceFragment class to load preferences.xml

--- app/src/main/java/es/pymasde/blueterm/PreferenceActivity.java
+++ app/src/main/java/es/pymasde/blueterm/PreferenceActivity.java
@@ -1,7 +1,39 @@
 package es.pymasde.blueterm;
 
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+import java.util.List;
+
 /**
  * Created by brettrosen on 3/14/16.
  */
 public class PreferenceActivity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+    /**
+     * Populate the activity with the top-level headers.
+     */
+    @Override
+    public void onBuildHeaders(List<android.preference.PreferenceActivity.Header> target) {
+    }
+
+    public static class PreferenceFragment extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(final Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+
+
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
+
+
 }
